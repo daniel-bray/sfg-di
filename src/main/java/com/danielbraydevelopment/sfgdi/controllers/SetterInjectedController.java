9b7a0da70670fa50d38f61cfa54e95b0e87c972e
kekkerslawl <braydj@gmail.com>
2020-04-12 23:05:51 -0500

    initial commit

---
+++ src/main/java/com/danielbraydevelopment/sfgdi/controllers/SetterInjectedController.java
@@ -0,0 +1,21 @@
+package com.danielbraydevelopment.sfgdi.controllers;
+
+import com.danielbraydevelopment.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SetterInjectedController {
+    private GreetingService greetingService;
+
+    @Qualifier("settingInjectedGreetingService")
+    @Autowired
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+}
